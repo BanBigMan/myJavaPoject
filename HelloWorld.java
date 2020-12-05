cae9091bd1f1884df8dc65c1a046a30031cade8e
BanBigMan <75526663+BanBigMan@users.noreply.github.com>
2020-12-05 20:06:03 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,20 @@
 public class HelloWorld{
-  public static void main(String[] agrs){
+  
+  //public 公众的
+  //static 静态的
+  //void   空的
+  //main   主要的
+  
+  public static void main(String[] agrs){   //主方法入口
+    
+    //System  系统的
+    //out     外面的
+    //print   打印（println）打印（ln）换行
+    
     System.out.println("Hello World!");
+    
+    //"这是字符串"，'这是字符'.
+    
   }
+  
 }
